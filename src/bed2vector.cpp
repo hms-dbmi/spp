45c6ddbe8bd91e1943417f85c23bb5d692fe5290
Peter Kharchenko <peter.kharchenko@gmail.com>
2011-08-18 13:57:07 +0000

    added local geline implementation

--- src/bed2vector.cpp
+++ src/bed2vector.cpp
@@ -22,6 +22,12 @@ extern "C" {
 #include "Rmath.h"
 #include "Rinternals.h"
 #include "Rdefines.h"
+  // for getline
+#include <stdio.h>   /* flockfile, getc_unlocked, funlockfile */
+#include <stdlib.h>  /* malloc, realloc */
+
+#include <errno.h>   /* errno */
+#include <unistd.h>  /* ssize_t */
 }
 
 using namespace std;
@@ -2625,4 +2631,71 @@ SEXP read_eland_multi(SEXP filename,SEXP read_tag_names_R,SEXP eland_tag_length_
 }
 
 
+}
+
+
+extern "C" ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+/* PASTE REMAINDER AT BOTTOM OF FILE */
+ssize_t
+getline(char **linep, size_t *np, FILE *stream)
+{
+  char *p = NULL;
+  size_t i = 0;
+
+  if (!linep || !np) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (!(*linep) || !(*np)) {
+    *np = 120;
+    *linep = (char *)malloc(*np);
+    if (!(*linep)) {
+      return -1;
+    }
+  }
+
+  flockfile(stream);
+
+  p = *linep;
+  for (int ch = 0; (ch = getc_unlocked(stream)) != EOF;) {
+    if (i > *np) {
+      /* Grow *linep. */
+      size_t m = *np * 2;
+      char *s = (char *)realloc(*linep, m);
+
+      if (!s) {
+        int error = errno;
+        funlockfile(stream);
+        errno = error;
+        return -1;
+      }
+
+      *linep = s;
+      *np = m;
+    }
+
+    p[i] = ch;
+    if ('\n' == ch) break;
+    i += 1;
+  }
+  funlockfile(stream);
+
+  /* Null-terminate the string. */
+  if (i > *np) {
+    /* Grow *linep. */
+      size_t m = *np * 2;
+      char *s = (char *)realloc(*linep, m);
+
+      if (!s) {
+        return -1;
+      }
+
+      *linep = s;
+      *np = m;
+  }
+
+  p[i + 1] = '\0';
+  return ((i > 0)? i : -1);
 }
